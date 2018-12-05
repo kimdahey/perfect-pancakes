c12c33054440cecf2ccb8e7cd75c9d5460c1dc6a
kimdahey <claire_kim1@brown.edu>
2018-12-05 15:57:57 -0500

    Updates

---
+++ precache-manifest.785790ea78e46da29b266510861bd565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "516ebe35e140e39b4d7a",
+    "url": "/perfect-pancakes/static/js/runtime~main.516ebe35.js"
+  },
+  {
+    "revision": "004b2a282e981e382052",
+    "url": "/perfect-pancakes/static/js/main.004b2a28.chunk.js"
+  },
+  {
+    "revision": "f25216638ebbcf4b8137",
+    "url": "/perfect-pancakes/static/js/1.f2521663.chunk.js"
+  },
+  {
+    "revision": "004b2a282e981e382052",
+    "url": "/perfect-pancakes/static/css/main.5900fab6.chunk.css"
+  },
+  {
+    "revision": "c7ccabd332e90b30cddb1000931422b2",
+    "url": "/perfect-pancakes/index.html"
+  }
+];
